9ca30bbb77e39c11e5d973f2250f2c2897dafdff
Felipe Segovia <fsegoviar@outlook.com>
2019-01-01 11:18:25 -0600

    Se agrega la clase Conector

---
+++ LibreriaTutInt/LibreriaTutInt/Conector.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "BaseDeConocimiento.h"
+#include "BaseDeHechos.h"
+#include "Hecho.h"
+#include "Regla.h"
+ref class Conector
+{
+public:
+	Conector();
+	~Conector();
+	Conector^ obtenerConector();
+	void agregarRegla(Regla^ regla);
+	void agregarHecho(Hecho^ hecho);
+	void borrarHechos();
+	bool getLeyendoRegla();
+	void inicioRegla();
+	void finRegla();
+	Regla^ transformarString_A_Regla(String^ regla);
+	BaseDeConocimiento^ obtenerBaseDeConocimiento();
+	BaseDeHechos^ obtenerBaseDeHechos();
+	void cerrarArchivo();
+	String^ pasarRegla_A_String(Regla^ regla);
+private:
+	Conector^ conector;
+	BaseDeConocimiento^ reglas;
+	BaseDeHechos^ hechos;
+	bool leyendoRegla = false;
+};
+
